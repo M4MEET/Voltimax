6d5968a01d98442b4d33d438353c5f71e4dcee66
M4MEET <imeetjoshi@gmail.com>
2022-11-14 17:48:12 +0100

    uploading the shopware theme for the voltimax.de

---
+++ src/VoltimaxTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace VoltimaxTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class VoltimaxTheme extends Plugin implements ThemeInterface
+{
+}
